6dfe95db5f92802db1a5de8df514182ad4d9b2c4
sarsenovic <sarsenovics@gmail.com>
2021-06-09 12:43:41 +0200

    Initial Commit

---
+++ Nothing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nothing
+//
+//  Created by Sasa Arsenovic on 09/06/2021.
+//  Copyright © 2021 Sasa Arsenovic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
